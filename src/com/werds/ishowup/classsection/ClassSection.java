379dca03b86af4670f146912e746a2ae757a3493
ychen0531 <chenyufei@live.com>
2013-11-05 05:06:48 -0600

    !

--- src/com/werds/ishowup/classsection/ClassSection.java
+++ src/com/werds/ishowup/classsection/ClassSection.java
@@ -3,6 +3,8 @@
  */
 package com.werds.ishowup.classsection;
 
+import org.json.*;
+
 /**
  * @author KevinC
  *
@@ -23,11 +25,50 @@ public class ClassSection {
 	private int endTime; // epoch time
 	private boolean[] weekdays;
 	
+	public ClassSection(String classData) throws JSONException {
+		importClass(classData);
+	}
+	
 	public void setTitle(String title, int number, String section) {
 		this.title = new String(title);
 		this.number = number;
 		this.section = new String(section);
 	}
 	
-	public void setLocation() {}
+	public void setLocation(String building, String room, double[] geoLocation) {
+		this.building = new String(building);
+		this.room = new String(room);
+		for (int i = 0; i < 2; i++) {
+			this.geoLoction[i] = geoLocation[i];
+		}
+	}
+	
+	public void setTime(int startTime, int endTime, boolean[] weekdays) {
+		this.startTime = startTime;
+		this.endTime = endTime;
+		for (int i = 0; i < 7; i++) {
+			this.weekdays[i] = weekdays[i];
+		}
+	}
+	
+	private void importClass(String sectionData) throws JSONException {
+		JSONObject classInfo = new JSONObject(sectionData);
+		JSONObject classTitle = classInfo.getJSONObject("Title");
+		JSONObject classLocation = classInfo.getJSONObject("Location");
+		JSONObject classTime = classInfo.getJSONObject("Time");
+		JSONArray classWeekdays = classTime.getJSONArray("WeekDays");
+		
+		this.title = new String(classTitle.getString("Title"));
+		this.number = classTitle.getInt("Number");
+		this.section = new String(classTitle.getString("Section"));
+		
+		this.building = new String(classLocation.getString("Building"));
+		this.room = new String(classLocation.getString("Room"));
+		this.weekdays = new boolean[7];
+		for (int i = 0; i < classWeekdays.length(); i++) {
+			//switch ()
+		}
+		
+	}
+	
 }
