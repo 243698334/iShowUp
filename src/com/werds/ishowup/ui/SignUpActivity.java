57fe4f8ba65f25a0b02e15957103f708f715a63b
chuang85 <chuang85@illinois.edu>
2013-12-11 21:56:15 -0600

    main ui color changed

--- src/com/werds/ishowup/ui/SignUpActivity.java
+++ src/com/werds/ishowup/ui/SignUpActivity.java
@@ -65,7 +65,6 @@ public class SignUpActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_signup);
 
 		// Get instances.
